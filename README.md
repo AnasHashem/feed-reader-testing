181796736003c4d7281de44edfb21e4221d302b1
Anas Hashem <34278087+AnasHashem@users.noreply.github.com>
2019-05-26 08:21:53 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Feed Reader Testing
+
+## Overview
+I built this project as a part of the front-end nanodegree program by Udacity. It involves writing test suites for JavaScript code using the Jasmine framework, which helps building applications using the test-driven approach.
+
+## Purpose
+Learning to apply test-driven programming and the Red-Green-Refactor Cycle to speed up the process of building error-free web applications.
+
+## Running the Script
+- Clone the repo and run the `index.html` file. 
+- Feel free to experiment and play with the test suites and specifications.
+
+## Contribution
+If you see any chance to improve the code, please do make a Pull Request.
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,83 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+$(function() {
+    
+    describe('RSS Feeds', function() {
+
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+        it(`URLs' are defined`, function() {
+            for (const feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            }
+        });
+
+        it(`names' are defined`, function() {
+            for (const feed of allFeeds){
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            }
+        });
+    });
+
+
+    describe(`The menu`, function(){
+
+        it ('is hidden', function() {
+            let bodyElement = document.getElementsByTagName('body')[0];
+            expect(bodyElement.classList.contains('menu-hidden')).toBe(true);
+        });
+
+        it(`toggles visibility`, function() {
+            let bodyElement = document.getElementsByTagName('body')[0];
+            let icon = document.querySelector('a.menu-icon-link');
+            icon.dispatchEvent(new Event('click'));
+            expect(bodyElement.classList.contains('menu-hidden')).toBe(false);
+            icon.dispatchEvent(new Event('click'));
+            expect(bodyElement.classList.contains('menu-hidden')).toBe(true);
+        });
+    });
+         
+
+    describe('Initial Entries', function() {
+        
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it('are there', function(done) {
+            expect(document.querySelector('.feed .entry')).toBeDefined();
+            done();
+        });
+    });
+        
+    
+    describe('New Feed Selection', function() {
+        let feed;
+
+        beforeEach(function(done){
+            loadFeed(0, function(){
+                feed = document.querySelector('.feed').innerHTML; //Original Feed
+                loadFeed(1, done);
+            });
+        });
+
+        it('content changes', function(done) {
+            //feed should've been change by now ..
+            expect(document.querySelector('.feed').innerHTML).not.toBe(feed);
+            done();
+        });
+    });
+}());
